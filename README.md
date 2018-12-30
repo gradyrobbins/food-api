c30405d31a30f678902d0fa32280af2323cc8391
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-30 12:03:13 -0600

    add styles.css and now api query is rendered onto DOM

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Food API
-##
-### Practice using Postman to perform GET, POST, PUT, and DELETE operations on your new food API.
+## Book3 - chapters 9 AND 10
+
+### ch 9 Practice using Postman to perform GET, POST, PUT, and DELETE operations on your new food API.
 
 1. [x] GET all curry dishes. Use the filter feature.
 1. [x] Use POST to create some of your favorite foods.
@@ -8,4 +9,12 @@
 1. [x] GET all foods and sort them by ethnicity.
 1. [x] Delete a food from the API by using the DELETE method, and putting in the URL of a specific resource (e.g. http://localhost:8008/food/2)
 
-# Practice complete
+# Chapter 9 practice complete
+
+### ch 10 Practice: Displaying Foods
+Once you have retrieved all of the foods from your own Food API, display each one of them in the DOM. Create an HTML representation of each food which will display the name of the food, its type, and its ethnicity.
+
+1. [x] Create a DOM element in your index.html with a class of foodList.
+1. [x] Create a function which returns a string template. The template is the HTML representation for a food item.
+1. [x] Create a function that inserts an HTML representation of a food into the DOM
+2. [ ] Use Flexbox row direction so that you have a horizontal list of items.
--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,10 +1,38 @@
 console.log("hello world foodapi.js");
 
+// Grab a reference to the container on the DOM
+let containerDiv = document.querySelector(".foodList");
+// test
+containerDiv.innerHTML = "Hello world -  container div";
+
+// Create a function that inserts an HTML representation of a food into the DOM; take 1 argument-the food object
+const addFoodToDom = (foodHTML) => {
+    containerDiv.innerHTML += foodHTML;
+};
+
+// Create a function which returns a string template. The template is the HTML representation for a food item.  This is borrowed from Emily L. and Megan C.
+const foodFactory = (foodObject) => {
+    let HTMLfoodSection =
+    `<section class="foodItemContainer">
+        <h2>${foodObject.name}</h2>
+        <p>${foodObject.type}</p>
+        <p>${foodObject.ethnicity}</p>
+        <p>${foodObject.country}</p>
+        <p>${foodObject.ingredients}</p>
+        <p>${foodObject.calories}</p>
+        <p>${foodObject.sugar}</p>
+        <p>${foodObject.fat}</p>
+    </section>`;
+    return HTMLfoodSection;
+};
+
+
+
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods);
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food);
+            addFoodToDom(foodAsHTML);
+        });
     });
-
-
-
